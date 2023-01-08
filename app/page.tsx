9c953e5b985d92eeb833095ec2d66af95f7521fc
Zan Thomson <zan@liven.com.au>
2023-01-08 21:54:43 +1100

    migrated to next13, removed styled components

---
+++ app/page.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import './globals.css'
+import ZanLogo from 'public/assets/zan.png';
+import styles from './page.module.css'
+import localFont from '@next/font/local'
+
+const snellBold = localFont({
+  src: "../public/assets/fonts/SnellBT-Bold.otf",
+  weight: '500'
+});
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.navigation}>
+        <div className={styles.logoContainer}>
+          <Image 
+            src={ZanLogo} 
+            fill={true} 
+            alt={'Zan logo'} 
+            priority={true} 
+            sizes="(max-width: 768px) 50vw,
+              (max-width: 1200px) 20vw"
+            />
+        </div>
+      </div>
+      <div className={styles.content}>
+      <div>
+        <div className={styles.headline}>
+          <h1><strong className={snellBold.className}>Z</strong>an Thomson</h1>
+        </div>
+        <p>Surfing curiosity. Playing with design, code, and meticulously crafted software. This is a canvas for my work, experiments, thoughts.</p>
+        <p>Currently: <strong>Product Lead at Liven.</strong> Building towards an independent practice. </p>
+      </div>
+      </div>
+      <div className={styles.links}>
+          <section>
+            <h3>Building</h3>
+            <hr/>
+            <a>Nomnie</a>
+          </section>
+          <section>
+            <h3>Exploring</h3>
+            <hr/>
+            <a>Design Systems</a>
+            <a>Brand Identity</a>
+            <a>Type Design</a>
+            <a>UI Engineering</a>
+          </section>
+          <section>
+            <h3>Writing</h3>
+            <hr/>
+            <a>Article 1</a>
+          </section>
+        </div>
+    </main>
+  )
+}
