d6baa0fd904b3c20d87670fae936cd98097809b5
Zan Thomson <zan@liven.com.au>
2023-01-14 11:46:23 +1100

    added parameters for textures

--- app/building/shiny/page.tsx
+++ app/building/shiny/page.tsx
@@ -14,7 +14,8 @@ import Foil from 'public/assets/textures/foil.png';
 import BulletTrain from 'public/assets/textures/bullet-train.jpg';
 import Image from 'next/image'
 import localFont from '@next/font/local';
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
+import { adjustHue } from 'polished';
 
 const kobataBold = localFont({
   src: "../../../public/assets/fonts/Kobata-Bold.otf",
@@ -26,32 +27,60 @@ const satoshiBold = localFont({
     weight: '700'
   });
 
+const mixBlendModeOptions = [
+    'normal',
+    'color',
+    'color-burn',
+    'color-dodge',
+    'multiply',
+    'screen',
+    'darken',
+    'lighten',
+    'overlay',
+    'difference',
+    'exclusion',
+    'soft-light',
+    'hard-light',
+    'hue',
+    'saturation',
+    'luminosity'
+]
+
 export default function Home() {
     const [mixBlendMode, setMixBlendMode] = useState('overlay')
-    const [brightness, setBrightness] = useState('200%')
-    const [contrast, setContrast] = useState('30%')
+    const [brightness, setBrightness] = useState('200')
+    const [contrast, setContrast] = useState('30')
     const [textureSrc, setTextureSrc] = useState(`url(${Grid.src})`)
+    const [backgroundGradient, setBackgroundGradient] = useState(true)
+    const [hue, setHue] = useState('245')
+
+    const gradientColor = adjustHue(22.5, `hsl(${hue}deg 90% 50%)`)
+    console.log('gradientColor:', gradientColor)
     
     const blendTextureParams = {
         '--mix-blend-mode': mixBlendMode,
-        '--brightness': brightness,
-        '--contrast': contrast,
-        '--texture-src': textureSrc
+        '--brightness': `${brightness}%`,
+        '--contrast': `${contrast}%`,
+        '--texture-src': textureSrc,
+        '--background': backgroundGradient? `radial-gradient(53.25% 53.25% at 25% 23%, ${gradientColor} 0%, ${gradientColor} 67.71%, hsl(${hue}deg 90% 50%) 100%)`:`hsl(${hue}deg 90% 50%)`,
+        '--text': `hsl(${hue}deg 50% 90%)`,
     }
+
+    function mixBlendModeChangeFunc() {
+        const mixBlendDropdown = document.getElementById("mixBlendDropdown");
+        if (mixBlendDropdown !== null) {
+            const selectedValue = mixBlendDropdown.options[mixBlendDropdown.selectedIndex].value;
+            setMixBlendMode(selectedValue)
+        }
+      }
+
     
     return (
         <main>
             <LeftNavigation returnPage={'/building'}/>
             <article>
-                <h1>Shiny elements</h1>
-                <p>I'm just going to experiment with framer animation in this page.</p>
-                <section>
-                    <ul className={styles.navBar}>
-                        <div>Lemon</div>
-                        <div>Tangerine</div>
-                        <div>Ladybug</div>
-                    </ul>
-                </section>
+                <h1>Sketchpad</h1>
+                <p>I'm just going to experiment with pseudo-selectors, textures, and Framer animation in this page.</p>
                 <section className={styles.section}>
                     <h2>Gradient borders</h2>
                     <p>To achieve the above result, we need a gradient border. But gradient borders aren't directly supported in CSS3. We're going to have to use pseudo-elements to achieve the result, via: <code>::before</code> and <code>::after</code>.</p>
@@ -75,12 +104,35 @@ export default function Home() {
                     <hr></hr>
                     <h3>Try it out</h3>
                     <hr></hr>
-                    <div className={styles.horizontalFlex}>
+                    <div className={`${styles.horizontalFlex}`}>
                         <div className={styles.colorTextureCard} style={blendTextureParams as React.CSSProperties}>
-                            <h3 className={`${satoshiBold.className} ${styles.demoText}`}>Yo<strong className={kobataBold.className}>u</strong> <strong className={kobataBold.className}>d</strong >on<strong className={kobataBold.className}>'</strong>t<br/>nee<strong className={kobataBold.className}>d</strong> <strong className={kobataBold.className}>a</strong>n<br/>ap<strong className={kobataBold.className}>p</strong>.</h3>
+                            <h3 className={`${satoshiBold.className} ${styles.demoText}`}>Yo<strong className={kobataBold.className}>u</strong> <strong className={kobataBold.className}>d</strong >on<strong className={kobataBold.className}>'</strong>t<br/>nee<strong className={kobataBold.className}>d</strong> <strong className={kobataBold.className}>a</strong>n<br/>app<strong className={kobataBold.className}>.</strong></h3>
                         </div>
-                        <div>
-                            Bleh
+                        <div className={`${styles.verticalFlex} ${styles.elevation} ${styles.fullWidth}`}>
+                            <div className={styles.formInputContainer}>
+                                <select value={mixBlendMode} id="mixBlendDropdown" onChange={(value) => mixBlendModeChangeFunc()}>
+                                    {mixBlendModeOptions.map((item, index)=>{
+                                        return <option key={index} value={item}>{item}</option>
+                                    })}
+                                </select>
+                                <label>Mix blend mode</label>
+                            </div>
+                            <div className={styles.formInputContainer}>
+                                <input type="range" min="1" max="300" value={brightness} id="brightnessSlider" step="10" onChange={(e) => setBrightness(e.target.value)}/>
+                                <label>Brightness</label>
+                            </div>
+                            <div className={styles.formInputContainer}>
+                                <input type="range" min="1" max="300" value={contrast} id="contrastSlider" step="10" onChange={(e) => setContrast(e.target.value)}/>
+                                <label>Contrast</label>
+                            </div>
+                            <div className={styles.formInputContainer}>
+                                <input type="range" min="1" max="360" value={hue} id="hueSlider" step="1" onChange={(e) => setHue(e.target.value)}/>
+                                <label>Hue</label>
+                            </div>
+                            <div className={styles.formInputContainer}>
+                                <input checked={backgroundGradient} type="checkbox" onChange={(e) => setBackgroundGradient(e.target.checked)}/>
+                                <label>Toggle gradient</label>
+                            </div>
                         </div>
                     </div>
                     <hr></hr>
